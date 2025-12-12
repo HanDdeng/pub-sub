c86cf368f9bc79d6c73ecad7cccc21fd9af31d70
HanDdeng <handdeng@GPDDMBP.local>
2025-12-12 17:57:33 +0800

    update: update-return-type*

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,10 @@ export type SubscribeOptions = {
 };
 
 export type Events<T> = Partial<Record<keyof T, { listener: T[keyof T]; options: SubscribeOptions }[]>>;
+
+export type PubSub<T extends BasePubSub> = {
+  subscribe: <K extends keyof T>(event: K, listener: T[K], options?: SubscribeOptions) => void;
+  unsubscribe: <K extends keyof T>(event?: K, listener?: T[K]) => void;
+  publish: <K extends keyof T>(event: K, ...args: Parameters<T[K]>) => void;
+  events: Events<T>;
+};
